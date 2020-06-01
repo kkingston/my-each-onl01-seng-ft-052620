435d379e4785380cf5e6c832af7675e3918f93d7
Kayla Kingston <kkingston09@gmail.com>
2020-06-01 23:04:51 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(words)
   counter = 0 
-  while counter < words.length 
-  yield words[counter]
-  counter += 1 
+    while counter < words.length 
+      yield words[counter]
+      counter += 1 
 end
 words
   
